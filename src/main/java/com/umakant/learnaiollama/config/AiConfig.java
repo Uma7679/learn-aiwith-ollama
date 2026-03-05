731f2800bd964d37bb4d297b275f46dd995eaf7d
Uma7679 <150547156+Uma7679@users.noreply.github.com>
2026-03-06 00:27:18 +0530

    applied prompt defaults

--- src/main/java/com/umakant/learnaiollama/config/AiConfig.java
+++ src/main/java/com/umakant/learnaiollama/config/AiConfig.java
@@ -2,6 +2,7 @@ package com.umakant.learnaiollama.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
     @Bean(name = "ollamaChatClient")
     public ChatClient ollamaChatModel(OllamaChatModel ollamaChatModel){
-        return ChatClient.builder(ollamaChatModel).build();
+        return ChatClient.builder(ollamaChatModel)
+                .defaultSystem("You are a cricket expert")
+                .defaultOptions(OllamaChatOptions.builder()
+                        .temperature(0.2)
+                        .build())
+                .build();
     }
 }
