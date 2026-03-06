43dcdf3b41ef78990fd5025c15669fada7680ee4
Uma7679 <150547156+Uma7679@users.noreply.github.com>
2026-03-06 23:42:10 +0530

    Implemented Advisor and Custom Advisor and log it

---
+++ src/main/java/com/umakant/learnaiollama/advisors/TokenPrintAdvisor.java
@@ -0,0 +1,56 @@
+package com.umakant.learnaiollama.advisors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+import reactor.core.publisher.Flux;
+
+
+public class TokenPrintAdvisor implements CallAdvisor, StreamAdvisor {
+
+    private Logger logger = LoggerFactory.getLogger(TokenPrintAdvisor.class);
+
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+
+        this.logger.info("My Token Print Advisor is called: ");
+        logger.info("Request: " + chatClientRequest.prompt().getContents());
+
+        ChatClientResponse chatClientResponse =  callAdvisorChain.nextCall(chatClientRequest);
+        logger.info("Token Advisor: Response recieved from the model");
+
+        logger.info("Response: " + chatClientResponse
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText());
+
+        logger.info("Total Token consumed: " + chatClientResponse
+                .chatResponse()
+                .getMetadata()
+                .getUsage()
+                .getTotalTokens());
+
+        return chatClientResponse;
+    }
+
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAdvisorChain streamAdvisorChain) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
--- src/main/java/com/umakant/learnaiollama/config/AiConfig.java
+++ src/main/java/com/umakant/learnaiollama/config/AiConfig.java
@@ -1,16 +1,22 @@
 package com.umakant.learnaiollama.config;
 
+import com.umakant.learnaiollama.advisors.TokenPrintAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class AiConfig {
     @Bean(name = "ollamaChatClient")
     public ChatClient ollamaChatModel(OllamaChatModel ollamaChatModel){
         return ChatClient.builder(ollamaChatModel)
+                .defaultAdvisors(new TokenPrintAdvisor(), new SimpleLoggerAdvisor(), new SafeGuardAdvisor(List.of("games")))
                 .defaultOptions(OllamaChatOptions.builder()
                         .format("json")
                         .temperature(0.8)
